32e8e7f13502a128c48b8359c36e46b198073963
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-09-04 11:19:42 +0700

    init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//sample http server for testing purposes
+	http.HandleFunc("/", ExampleHandler)
+	log.Println("** Service Started on Port 8080 **")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, `{"status":"ok"}`)
+}
